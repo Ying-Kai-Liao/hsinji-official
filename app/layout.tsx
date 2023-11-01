aac6427008968408bf4913bfe544aeb0f31a2bff
Ying-Kai Liao <yklworkingaddress@gmail.com>
2023-11-01 11:52:45 +0800

    font updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import './font.css'
 import type { Metadata } from 'next'
 import NavBar from '@/components/navbar/NavBar'
 import Footer from '@/components/footer/Footer'
@@ -7,12 +8,6 @@ import React from 'react'
 export const metadata: Metadata = {
   title: '信基診所',
   description: '創新醫療，為您效勞',
-  icons: {
-    other: {
-      rel: 'stylesheet',
-      url: 'https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;200;300;400;500;600;700;800;900&family=Noto+Serif+TC:wght@200;300;400;500;600;700;900&display=swap',
-    },
-  },
 }
 
 export default function RootLayout({
